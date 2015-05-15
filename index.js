20c9bdfff1e109bc9f2f5bbadf7154b1ab89d7c8
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-05-14 19:21:34 -0700

    creating files for FP

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ var hapi = require("hapi");
 var server = new hapi.Server();
 server.connection({ port: 8000 });
 server.start();
+
+server.views({
+  path: ".",
+  engines: {
+    html: require("handlebars")
+  },
+isCached: false
+})
+
+server.route({
+  method: "GET",
+  path: "/",
+  handler: function(req, reply){
+    reply.view(index, (title: "Hello Bloggers"))
+  }
+});
+
+server.route({
+  method: "GET",
+  path: "/public/{param*}",
+  handler: {
+    directory: {
+      path: "build"
+    }
+  }
+});
