2792dc8d2debbf7068b860574edea1f5b7de2b89
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-05-26 07:48:43 -0700

    add files

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//index.js
+
 var hapi = require("hapi");
 
 var server = new hapi.Server();
@@ -5,32 +7,43 @@ server.connection({port: 8000});//listen using the connection function
 server.start();
 
 server.views({
-  path: ".",
+  path: ".", //"templates"
   engines: {
     html: require("handlebars")
   },
 isCached: false
 })
 //register the routes, once matched, I want a response
+
+//var counter = 0;//this state lives outside the route/request, counter will stick around and the value will reset; see line 32, 33
+
 server.route({
   method: "GET", //use a method - GET, POST, PUT, DELETE
-  path: "/", //route path
+  path: "/", //route path, i.e., {name?} name must be there or undefined
+  //path: "/{name?}",
   handler: function(request, reply){ //function called when it gets request from outside, 2 argu
-  //  reply.view(index, {
-  //    title: "Hello Bloggers"
-  //  });
-    console.log(request.headers);
-    console.log(request.params);
-    reply("Hello from me");
-  }
-});
+    reply.view(index, {  //load index off the hardrive and use it
+      title: "Hello Bloggers" //
+    });
+  //console.log(request.headers);
+  //console.log(request.params);
+    //reply("Hello, " + request.params.name + ", from me"); //pre-sanitized for us
 
-//server.route({
-//  method: "GET", //
-//  path: "/public/{param*}",//matches the name in your html files
-//  handler: function(request, reply){
-//    directory: {
-//      path: "build"
-//    }
-//  }
-//});
+  //var name = request.params.name ||
+    //"Anonymous";   // typical way to handle defaults
+    //counter++; //adds a counter everytime you call the function -> +1
+    //reply("Hello, " + name + ", from me " + counter);//change the reply statement for request
+  }
+});//end route to index
+
+ server.route({
+   method: "GET", //
+    path: "/public/{param*}",//matches the name in your html files
+    //path: "/{name}/{id}",
+    handler: function(request, reply){
+      directory: {
+        path: "build"
+        //reply(request.params.name + "|" + request.params.id);
+    }
+   }
+ });//end route to build
