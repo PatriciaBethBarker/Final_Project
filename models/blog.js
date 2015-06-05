42f8120f13dc723b591bf5f2e5af4520b6697a18
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-05 13:42:32 -0700

    add models to Final Project

---
+++ models/blog.js
@@ -0,0 +1,42 @@
+//blog.js
+
+var Backbone = require("backbone";
+var db = require("../db");\
+
+var LOAD = "SELECT name, title, author FROM blogs WHERE rowid = $id;";
+var SAVE_NEW = "INSERT INTO blogs (name, title, author) VALUES ($name, $title, $author);";
+var UPDATE = "UPDATE blogs SET name = $name, title = $title, author = $author WHERE rowid = $id;";
+var LAST = "SELECT last_insert_rowid() AS rowid FROM blogs;";
+
+module.exports = Backbone.Model.extend({
+  defaults: {
+    name: "Untitled Blog",
+    title: "",
+    author: "",
+    id: "new"
+  },
+  load: function(done) {
+    var self: this;
+    var query = db.connection.prepare(LOAD);
+    var data = this.toJASON();
+    query.get({
+      $id: data.id
+    }, function(err, loaded) {
+      self.set(loaded);
+      done(err);
+    });
+  },
+  save: function(done) {
+    var self = this;
+    var id = this.get("id");
+    var q = id == "new" ? SAVE_NEW : UPDATE;
+    var query = db.connection.prepare(q);
+    var data = this.toJASON();
+    query.run({
+      $name: data.name,
+      $title: data.title,
+      $author: data.author,
+      $id: id == "new" ? undefined : data.id
+    }, done);
+  }
+});
