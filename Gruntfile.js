fd765ef22879ff33d75eea4cf2c9a83e1dfe9f57
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-07 20:02:01 -0700

    add build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,26 @@
 //Gruntfile.js
 module.exports = function(grunt) {
   //create a task and add them in order
+
+  //locate all JS files inside src.js
+  //  "src/js/**/*.js"
+  //will find
+  //  src/js/Lib/share.min.js
+  //  src/js/min.js
+  //will not find:
+  //  src/js/index.html
+  //  src/404.js
+  
+
   grunt.registerTask("hello",
   function() {//provide an array of other task names
     console.log("Hello, I'm using grunt!");
+    grunt.file.write("build/test.txt", "This file is written sync");//this creates the folders and the file - directory is written here
   });
-  //ie, less css files, autoprefixer, insequence
+
+  //good to call up assets
+
+  //ie, less css files, autoprefixer, in sequence
   grunt.registerTask("hi", ["hello"]);
 
 };
