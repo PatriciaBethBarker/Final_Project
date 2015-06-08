28c99dc3db7f09ddb3381300ae33ba3c73e467cf
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-07 20:18:08 -0700

    working new changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,17 +10,30 @@ module.exports = function(grunt) {
   //will not find:
   //  src/js/index.html
   //  src/404.js
-  
+
 
   grunt.registerTask("hello",
   function() {//provide an array of other task names
     console.log("Hello, I'm using grunt!");
-    grunt.file.write("build/test.txt", "This file is written sync");//this creates the folders and the file - directory is written here
+    grunt.file.write("build/test.txt", "This file is written sync");//this creates the folders and the file -
+    //directory is written here
   });
 
   //good to call up assets
 
   //ie, less css files, autoprefixer, in sequence
   grunt.registerTask("hi", ["hello"]);
+
+  grunt.loadNpmTask("grunt-autoprefixer");
+
+  grunt.initConfig({
+    autoprefixer: {
+      dev: {
+        src: "public/css/**/*.css",
+        dest: "build/css"
+      }
+    }
+
+  });
 
 };
