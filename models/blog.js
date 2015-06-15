4a5ec6ef7f457ad9b6dac33e5ebd92e358ad920a
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-14 18:11:13 -0700

    add blog values to db sql statement

--- models/blog.js
+++ models/blog.js
@@ -4,7 +4,7 @@ var Backbone = require("backbone";
 var db = require("../db");\
 
 var LOAD = "SELECT name, title, author FROM blogs WHERE rowid = $id;";
-var SAVE_NEW = "INSERT INTO blogs (name, title, author) VALUES ($name, $title, $author);";
+var SAVE_NEW = "INSERT INTO blogs (name, title, author, content, category, tags, meta, created_at, updated_at) VALUES ($name, $title, $author, $content, $category, $tags, $meta, $created_at, $updated_at);";
 var UPDATE = "UPDATE blogs SET name = $name, title = $title, author = $author WHERE rowid = $id;";
 var LAST = "SELECT last_insert_rowid() AS rowid FROM blogs;";
 
@@ -13,6 +13,12 @@ module.exports = Backbone.Model.extend({//Backbone models are "observable:"
     name: "Untitled Blog",
     title: "",
     author: "",
+    content: "",
+    category: "",
+    tags: "",
+    meta: "",
+    created_at: "",
+    updated_at: "",
     id: "new"
   },//they fire events when their properties are changed
   load: function(done) {
@@ -36,6 +42,12 @@ module.exports = Backbone.Model.extend({//Backbone models are "observable:"
       $name: data.name,
       $title: data.title,
       $author: data.author,
+      $content: data.content,
+      $category: data.category,
+      $tags:  data.tags,
+      $meta:  data.meta,
+      $created_at: data.created_at,
+      $updated_at: data.updated_at,
       $id: id == "new" ? undefined : data.id
     }, done);
   }
