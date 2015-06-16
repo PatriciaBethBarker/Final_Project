1b65fb93819c3aad3cd05975c5be9bab2715c2fc
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-16 15:06:41 -0700

    remove extra charatchers

--- routes.js
+++ routes.js
@@ -3,35 +3,35 @@ module.exports = [{
 //build the routes
   path: "/",
   method: "GET",
-  handler: require("./handlers/home") {
+  handler: require("./handlers/home") 
 }, {
   path: "/posts", //duplicate of home path
   method: "GET",
-  handler: require("./handlers/home") {
+  handler: require("./handlers/home") 
 }, {
   path: "/posts/new",//new post
   method: "GET",
-  handler: require("./handlers/addPost") {
+  handler: require("./handlers/addPost") 
 }, {
   path: "posts/{slug}/edit",//edit post
   method: "GET",
-  handlers: require("./handlers/editPost") {
+  handlers: require("./handlers/editPost") 
 }, {
   path: "posts/{slug}",//save post
   method: "POST",//post saved
-  handlers: require("./handlers/savePost") {
+  handlers: require("./handlers/savePost") 
 }, {//create single page view
   path: "posts/{slug}",//return name of post
   method: "GET",
-  handlers: require("./handlers/viewPost") {
+  handlers: require("./handlers/viewPost") 
 }, {
   path: "/login",//login
   method: "GET",
-  handlers: require("./handlers/getLogin") {
+  handlers: require("./handlers/getLogin") 
 }, {
   path: "/login",
   method: "POST",
-  handlers: require("./handlers/postLogin") {
+  handlers: require("./handlers/postLogin") 
 }, {
   path: "/assets/{param*}",
   method: "GET",
