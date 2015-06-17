871a91151063964baf07261bae3e9df2ff3a6c1e
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-17 16:53:54 -0700

    add if, redirect to login

--- handlers/home.js
+++ handlers/home.js
@@ -5,7 +5,12 @@ module.exports = function(req, reply) {
   db.getAllPosts(function(err, posts) {
     posts.forEach(function(post) {
       post.truncated = post.content.substr(0, 2);//shorten the content
-    })
+    });
+
+    if (!req.state.user) {
+      return reply.redirect("/login");
+    }
+    
     reply.view("index", {
       posts: posts,
       title: "Home",
