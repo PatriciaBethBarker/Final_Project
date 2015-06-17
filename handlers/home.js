9586cc3911a2be8c8a32fdeb19342b60ccec602c
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-17 16:24:39 -0700

    add images folder

--- handlers/home.js
+++ handlers/home.js
@@ -2,7 +2,7 @@
 var db = require("../db");
 
 module.exports = function(req, reply) {
-  db.getAllBlogs(function(err, posts) {
+  db.getAllPosts(function(err, posts) {
     posts.forEach(function(post) {
       post.truncated = post.content.substr(0, 2);//shorten the content
     })
--- handlers/postLogin.js
+++ handlers/postLogin.js
@@ -1,28 +1,28 @@
 //postLogin.js
 var db = require("../db");
-var Crypto = require("crypto");
+var crypto = require("crypto");
 
 module.exports = function (req, reply) { //create hash varible
-  var sha1 = Crypto.createHash("sha1");
+  var hash = crypto.createHash("sha1");
 
   //get password/username from payload, add connection
   db.connection.get("SELECT * FROM users WHERE username = $username", {
     $username: req.payload.name
   }, function(err, expected) {  //match the password from db
-    
+
       console.log(req.payload, expected, err);
         //if, else statement
-        
+
         if (expected && req.payload.password == expected.password) {
-          
+
           var response = reply.redirect("/");
           var id = req.payload.name + Date.now();
-          sha1.update(id);
-          id = sha1.digest("hex");
+          hash.update(id);
+          id = hash.digest("hex");
           response.state("user", req.payload.name);
           response.state("session", id);
           console.log(req.payload.name, id);
-          
+
           //make the db connection
           db.connection.run("UPDATE users SET session = $session WHERE username = $user", {
             $user: req.payload.name,
