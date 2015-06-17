ed2710abfa7ffab872adcbd033cde567db8d84a5
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-16 20:13:31 -0700

    changes

--- models/post.js
+++ models/post.js
@@ -4,20 +4,18 @@ var db = require("../db");
 var moment = require("moment");
 
 var LOAD = "SELECT name, title, author FROM posts WHERE slug = $slug;";
-var SAVE_NEW = "INSERT INTO posts (name, title, author, content, category, created_at, formatted) VALUES ($name, $title, $author, $content, $category, $datetime('now'), $formatted);";
-var UPDATE = "UPDATE posts SET name = $name, title = $title, author = $author WHERE slug = $slug;";
+var SAVE_NEW = "INSERT INTO posts (slug, title, author, content, created_at, formatted) VALUES ($slug, $title, $author, $content, datetime('now'), $formatted);";
+var UPDATE = "UPDATE posts SET title = $title, author = $author WHERE slug = $slug;";
 var LAST = "SELECT last_insert_rowid() AS rowid FROM posts;";
 
 //Backbone models are observable
 module.exports = Backbone.Model.extend({
   defaults: {
-    name: "Untitled Post",
     title: "",
     author: "",
     content: "",
-    category: "",
     created_at: "",
-    id: "new" 
+    id: "new"
   },//they fire events when their properties are changed
   load: function(done) {
     var self = this;
@@ -33,7 +31,15 @@ module.exports = Backbone.Model.extend({
   save: function(done) {
     var self = this;
     var id = this.get("id");
-    var q = id == "new" ? SAVE_NEW : UPDATE;
+      if ("") {
+
+
+      } else {
+
+
+      }
+    
+    //var q = id == "new" ? SAVE_NEW : UPDATE;
     var query = db.connection.prepare(q);
     var data = this.toJSON();
     var slug = this.get("title").toLowerCase();
