6b9ea5faebadacdb43cdbd893df798423f0242d6
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-17 16:28:03 -0700

    changes

--- handlers/editPost.js
+++ handlers/editPost.js
@@ -6,6 +6,12 @@ module.exports = function(req, reply) {
     var post = new Post({
         slug: req.params.slug
     });
+
+    //repeat same state as home
+    if (!req.state.user) {//send to Login
+      return reply.redirect("/login");
+    }
+
     post.load(function() {
         reply.view("post", {
             title: "Edit post",
