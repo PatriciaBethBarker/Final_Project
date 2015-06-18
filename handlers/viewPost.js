078809f7497513e464efd32f8a24c2ee156aaa40
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-17 17:30:24 -0700

    clean up code

--- handlers/viewPost.js
+++ handlers/viewPost.js
@@ -6,10 +6,11 @@ module.exports = function(req, reply) {
     var post = new Post({
         slug: req.params.slug
     });
+
     post.load(function() {
         reply.view("view", {
             title: post.get("title"),
             post: post.toJSON()
         })//view post
-   })
+    })
 };
