f123a56755380285ba526c65ff1254226020792a
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-17 21:35:10 -0700

    fix the db err

--- models/post.js
+++ models/post.js
@@ -3,21 +3,22 @@ var Backbone = require("backbone");
 var db = require("../db");
 var moment = require("moment");
 //models with key-value binding and custom events
-var LOAD = "SELECT name, title, author FROM posts WHERE slug = $slug;";
+var LOAD = "SELECT * FROM posts WHERE slug = $slug;";
 var SAVE_NEW = "INSERT INTO posts (slug, title, author, content, created_at, formatted) VALUES ($slug, $title, $author, $content, datetime('now'), $formatted);";
-var UPDATE = "UPDATE posts SET title = $title, author = $author WHERE slug = $slug;";
+var UPDATE = "UPDATE posts SET title = $title, content = $content WHERE slug = $slug;";
 var LAST = "SELECT last_insert_rowid() AS rowid FROM posts;";
 
 //Backbone models are observable
 module.exports = Backbone.Model.extend({
   defaults: {
-    title: "New posts",
-    author: "Author Name",
+    name: "Untitled Post",
+    title: "",
+    author: "Author",
     content: "",
-    created_at: "",
-    id: "new"
+    //created_at: "",
+    slug: "new"
   },//they fire events when their properties are changed
-  load: function(done) {  //load
+  load: function (done) {  //load
     var self = this;
     var query = db.connection.prepare(LOAD);
     var data = this.toJSON();
@@ -28,34 +29,33 @@ module.exports = Backbone.Model.extend({
       done(err);
     });
   },
-  save: function(done) { //save
+  save: function (done) { //save
     var self = this;
-    var id = this.get("id");
-
-
-      //if statement
-      if (id == "new") {
-        var query = db.connection.prepare(q);
-        var data = this.toJSON();
-        var slug = this.get("title").toLowerCase();
-        console.log(data);
+    //if statement
+    if (this.get("slug") == "new") {
+      var query = db.connection.prepare(SAVE_NEW);//fix the db err
+      var data = this.toJSON();
+      var slug = this.get("title").toLowerCase();
+      // console.log(data);
+      var space = /\s/g;
+      slug = slug.replace(space, "-");
 
         query.run({
           $title: data.title,
           $author: data.author,
           $content: data.content,
-          $id: id == "new" ? undefined : data.id,//format date
           $formatted: moment().format("MMMM Do YYYY, h:mm:ss a"),
           $slug: slug
         }, done);
 
       } else { //update
+      var query = db.connection.prepare(UPDATE);//fix the db err
+      var data = this.toJSON();
 
         query.run({
           $title: data.title,
           $content: data.content,
-          $id: id == "new" ? undefined : data.id,
-          $slug: slug
+          $slug: data.slug
         }, done);
       }
   }
