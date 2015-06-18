ac26133201743d2b10f51ccae09b67ca3f511800
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-17 17:15:25 -0700

    clean up code

--- handlers/savePost.js
+++ handlers/savePost.js
@@ -6,10 +6,11 @@ module.exports = function(req, reply) {
   var payload = req.payload;
   var model = new Post(payload);
   model.save(function(err) {
+
     if (err) {
       console.error(err);
     }
-    
-    reply.redirect("/posts/");
+
+    reply.redirect("/posts");
   });
 };
