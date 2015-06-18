41adbb98fa3f0e1ad77bf7117a6d617a7f8ff773
Patricia Beth Barker <patriciabethbarker@gmail.com>
2015-06-17 17:10:31 -0700

    add if else statement

--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@
 var Backbone = require("backbone");
 var db = require("../db");
 var moment = require("moment");
-
+//models with key-value binding and custom events
 var LOAD = "SELECT name, title, author FROM posts WHERE slug = $slug;";
 var SAVE_NEW = "INSERT INTO posts (slug, title, author, content, created_at, formatted) VALUES ($slug, $title, $author, $content, datetime('now'), $formatted);";
 var UPDATE = "UPDATE posts SET title = $title, author = $author WHERE slug = $slug;";
@@ -11,13 +11,13 @@ var LAST = "SELECT last_insert_rowid() AS rowid FROM posts;";
 //Backbone models are observable
 module.exports = Backbone.Model.extend({
   defaults: {
-    title: "",
-    author: "",
+    title: "New posts",
+    author: "Author Name",
     content: "",
     created_at: "",
     id: "new"
   },//they fire events when their properties are changed
-  load: function(done) {
+  load: function(done) {  //load
     var self = this;
     var query = db.connection.prepare(LOAD);
     var data = this.toJSON();
@@ -28,28 +28,31 @@ module.exports = Backbone.Model.extend({
       done(err);
     });
   },
-  save: function(done) {
+  save: function(done) { //save
     var self = this;
     var id = this.get("id");
-      if ("") {
+      if (id == "new") {
+        var query = db.connection.prepare(q);
+        var data = this.toJSON();
+        var slug = this.get("title").toLowerCase();
 
+        query.run({
+          $title: data.title,
+          $author: data.author,
+          $content: data.content,
+          $id: id == "new" ? undefined : data.id,//format date
+          $formatted: moment().format("MMMM Do YYYY, h:mm:ss a"),
+          $slug: slug
+        }, done);
 
-      } else {
+      } else { //update
 
-
+        query.run({
+          $title: data.title,
+          $content: data.content,
+          $id: id == "new" ? undefined : data.id,
+          $slug: slug
+        }, done);
       }
-    
-    //var q = id == "new" ? SAVE_NEW : UPDATE;
-    var query = db.connection.prepare(q);
-    var data = this.toJSON();
-    var slug = this.get("title").toLowerCase();
-
-    query.run({
-      $title: data.title,
-      $content: data.content,
-      $id: id == "new" ? undefined : data.id,//format date
-      $formatted: moment().format("MMMM Do YYYY, h:mm:ss a"),
-      $slug: slug
-    }, done);
   }
 });
